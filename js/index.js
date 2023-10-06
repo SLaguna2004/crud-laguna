62276b0164f9448c3cdf0ef445b8c51fc708cc97
Samuel <Samuel2219901@gmail.com>
2023-10-05 20:02:28 -0500

    todo

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ function app(){
     crud.create([7, 12, 8]);
     crud.create({ message: "Hola mundo"});
 
+    crud.update(6, [1, 5, 3]);
+
+    crud.delete(4);
+
     let crud2 = new CRUD("ejemplo");
     console.log(crud2.readAll());
 }
